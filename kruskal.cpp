c00a92fdf128d8d959fa68ae9fe2d5655ee7c2a2
SuJin <1127sujink@naver.com>
2021-11-22 23:53:11 +0900

    Create kruskal.cpp

---
+++ kruskal.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+void kruskal(int n, int m, char E, char F)
+{
+    // index i, j;
+    int i, j;
+    // set_pointer p, q;
+    char p, q;
+    // edge e;
+    int e;
+
+    // sort the m edges in E by weight in nondecreasing order;
+    // F = 공집합;
+
+    // initial(n);
+
+    while (F < n-1)
+    {
+        // e = edges with least weight not yet considered;
+        // i, j = indices of vertices connected by e;
+        // p = find(i);
+        // q = find(j);
+        // if(!equal(p,q)){
+        //     merge(p,q);
+        //     add e to F;
+        // }
+    }
+}
