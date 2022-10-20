ea1d536a51af382f8f57d89b6bcd6293b653ec1d
SuJin <1127sujink@naver.com>
2022-10-20 09:46:11 +0900

    221020
    runtime이 오래 걸릴 것 같아서 수정이 필요할 것 같다

---
+++ JavaScript/largestProductOfThree.js
@@ -0,0 +1,22 @@
+/**
+ * 정수를 요소로 가지는 배열을 입력받아 3개의 요소를
+ * 곱해 나올 수 있는 최대값을 리턴
+ */
+
+const largestProductOfThree = function (arr) {
+    var max = arr[0] * arr[1] * arr[2];
+    // 길이만큼 for문 돌리고
+    // 3번만 곱하는데
+    // 값이 가장 큰걸 업데이트 해주는...
+    for(let x = 0; x < arr.length; x++){
+      for(let y = 1 + x; y < arr.length; y++){
+        for(let z = 2 + x; z < arr.length; z++){
+          if(max < arr[x] * arr[y] * arr[z] && x !== y && y !== z && x !== z) {
+            max = arr[x] * arr[y] * arr[z];
+          }
+        }
+      }
+    }
+    return max;
+  };
+  
