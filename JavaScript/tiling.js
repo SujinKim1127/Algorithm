885319d23c3f5686e1063b4efbe0acc98db21af3
SuJin <1127sujink@naver.com>
2022-11-02 09:32:47 +0900

    221102
    다른 방식 업데이트

--- JavaScript/tiling.js
+++ JavaScript/tiling.js
@@ -16,3 +16,30 @@
   
   };
   
+// dynamic with tabulation: O(N)
+// tabulation: 데이터를 테이블에 정리하면서 bottom-up 방식으로 해결하는 것
+let tiling_2 = function (n) {
+  const memo = [0, 1, 2];
+  if (n <= 2) return memo[n];
+  for(let size = 3; size <= n; size++){
+    memo[size] = memo[size - 1] + memo[size - 2]
+  }
+  return memo[n]
+}
+
+// dynamic with slicing window: O(N)
+// slicing window: 필요한 최소한의 데이터만 활용
+// n의 문제에 대한 해결을 위해 필요한 데이터는 오직 2개뿐이다
+let tiling_3 = function (n) {
+  let first = 1, second = 2;
+  if(n <= 2) return n;
+  for(let size = 3; size <= n; size++){
+    // 앞의 두 수를 더하여 다음 수 구하기
+    const next = first + second;
+
+    // 다음 문제로 넘어가기 위해 필요한 2개의 데이터 순서 정리
+    fisrt = second;
+    second = next;
+  }
+  return second;
+}
