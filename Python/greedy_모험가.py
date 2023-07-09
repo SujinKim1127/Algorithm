6de85ee332c981414da9781fd0f898f53f3c6704
SuJin <kimsujin001127@gmail.com>
2023-07-09 22:44:31 +0900

    [TIL] greedy

--- Python/greedy_모험가.py
+++ Python/greedy_모험가.py
@@ -1,12 +1,31 @@
 n = int(input())
 
-data = list(map(int, input().split()))
+data = []
+for i in range(n):
+    data.append(int(input()))
 
-data.sort(reverse=True)
+data.sort(reverse=True)  # 내림차순
 
 result = 0
 while ( len(data) > 0 ):
     del data[0:data[0]]
     result += 1
+
+print(result)
+
+
+## answer
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+result = 0
+count = 0
+
+for i in data:
+    count += 1
+    if count > i:
+        result += 1
+        count = 0
 
 print(result)
