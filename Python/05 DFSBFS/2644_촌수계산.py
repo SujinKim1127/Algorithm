4a10ada1fb71b985a769ac53e3381476f3cc22a2
SuJin <kimsujin001127@gmail.com>
2024-03-11 15:08:56 +0900

    백준 DFS 실2

--- Python/05 DFSBFS/2644_촌수계산.py
+++ Python/05 DFSBFS/2644_촌수계산.py
@@ -1,31 +1,26 @@
 n = int(input())
-end, start = map(int, input().split())
+first, second = map(int, input().split())
 m = int(input())
-
-visited = [False] * (n + 1)
+visited =  [False] * (n + 1)
 graph = [[False] * (n + 1) for _ in range(n + 1)]
 
-for i in range(m):
+for _ in range(m):
     x, y = map(int, input().split())
     graph[x][y] = True
     graph[y][x] = True
 
+cnt = 0
 result = []
-
-def dfs(v, cnt):
+def DFS(v, cnt):
     cnt += 1
-    visited[v] = True
-    if v == end: 
+    visited[v]= True
+    if v == second:
         result.append(cnt)
     else:
         for i in range(1, n + 1):
             if visited[i] == False and graph[v][i]:
-                dfs(i, cnt)
+                DFS(i, cnt)
 
-dfs(start, 0)
-
+DFS(first, 0)
 if(len(result) == 0): print(-1)
 else: print(result[0] - 1)
-
-
-## bfs로도 풀어보기!!!
