bd541372794471bb1273141247e158ae2ebbaf50
SuJin <kimsujin001127@gmail.com>
2024-03-12 16:22:11 +0900

    백준 DFS 실2

---
+++ Python/05 DFSBFS/1012_유기농배추.py
@@ -0,0 +1,32 @@
+import sys 
+sys.setrecursionlimit(10000) 
+
+t = int(input())
+
+dx = [1, -1, 0 , 0]
+dy = [0, 0, 1, -1]
+
+def DFS(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if(0 <= nx < n) and (0 <= ny < m):
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = -1
+                DFS(nx, ny)
+
+for _ in range(t):
+    m,n,k = map(int, input().split())
+    graph = [[0] * (m) for _ in range(n)]
+    cnt = 0
+    for _ in range(k):
+        x, y = map(int, input().split())
+        graph[y][x] = 1
+    for i in range(n):
+        for j in range(m):
+            if(graph[i][j] > 0 ):
+                DFS(i, j)
+                cnt += 1
+
+    print(cnt)
