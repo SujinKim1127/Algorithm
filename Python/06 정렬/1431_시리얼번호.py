ebb253c16e5274e9108781a6eefa11aa35a085f7
SuJin <kimsujin001127@gmail.com>
2024-03-25 20:19:57 +0900

    백준 정렬 실3

---
+++ Python/06 정렬/1431_시리얼번호.py
@@ -0,0 +1,20 @@
+# n = int(input())
+# arr = []
+# for _ in range(n):
+#     arr.append(input())
+
+# def sum_num(input):
+#     result = 0
+#     for i in input:
+#         if i.isdigit():
+#             result += int(i)
+#     return result
+
+# arr.sort(key=lambda x:(len(x), sum_num(x), x))
+
+# for x in arr:
+#     print(x)
+
+arr = [1,2,3,4,5,6]
+new = arr[2:5]
+print(new)
