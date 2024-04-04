30ad06903b2b733fd413aa30fed7a94f0891a2df
SuJin <kimsujin001127@gmail.com>
2024-04-04 10:57:15 +0900

    백준 BFS 실4

--- Python/05 DFSBFS/16173_점프왕쩰리.py
+++ Python/05 DFSBFS/16173_점프왕쩰리.py
@@ -19,3 +19,44 @@ def dfs(x, y):
 dfs(0,0)
 if visit[-1][-1] == 1: print("HaruHaru")
 else: print("Hing")
+
+
+from collections import deque
+n = int(input())
+graph = []
+
+for _ in range(n):
+    graph.append(list(map(int, input().split())))   
+
+visit = [[False]*n for _ in range(n)]
+
+dx = [1, 0]
+dy = [0, 1]
+
+def bfs(x, y):
+    q = deque()
+    q.append([x,y])
+
+    while q:
+        x, y = q.popleft()
+        step = graph[x][y]  # 현재 위치 값
+
+        if graph[x][y] == -1:
+            return True
+        for move in range(2):
+            nx = x + dx[move] * step
+            ny = y + dy[move] * step
+
+            # 범위 벗어남
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
+            if not visit[nx][ny]:
+                visit[nx][ny] = True
+                q.append([nx,ny])
+
+if bfs(0, 0):
+    print('HaruHaru')
+else: print('Hing')
+
+
+
