5bf9b2f315ff77b27c056a1dde8d1d188be5e754
SuJin <kimsujin001127@gmail.com>
2024-04-11 18:03:51 +0900

    백준 BFS 실2

--- Python/05 DFSBFS/1012_유기농배추.py
+++ Python/05 DFSBFS/1012_유기농배추.py
@@ -30,3 +30,34 @@ for _ in range(t):
                 cnt += 1
 
     print(cnt)
+
+from collections import deque
+
+
+def BFS(x, y):
+    queue = deque()
+    queue.append([x, y])
+    graph[x][y] = -1
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if(0 <= nx < m) and (0 <= ny < n) and graph[nx][ny] == 1:
+                    graph[nx][ny] = -1
+                    queue.append([nx, ny])
+    
+for _ in range(t):
+    m,n,k = map(int, input().split())
+    graph = [[0] * (n) for _ in range(m)]
+    cnt = 0
+    for _ in range(k):
+        x, y = map(int, input().split())
+        graph[x][y] = 1
+    for i in range(m):
+        for j in range(n):
+            if(graph[i][j] > 0 ):
+                BFS(i, j)
+                cnt += 1
+    print(cnt)
