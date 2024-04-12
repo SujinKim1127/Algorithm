dc468d64691c3d42149fac6251b9c89cf9fa7e2b
SuJin <kimsujin001127@gmail.com>
2024-04-12 22:10:34 +0900

    백준 BFS 실2

--- Python/05 DFSBFS/2644_촌수계산.py
+++ Python/05 DFSBFS/2644_촌수계산.py
@@ -1,26 +1,51 @@
 n = int(input())
 first, second = map(int, input().split())
 m = int(input())
-visited =  [False] * (n + 1)
-graph = [[False] * (n + 1) for _ in range(n + 1)]
+
+visited =  [-1] * (n + 1)
+graph = [[] * (n + 1) for _ in range(n + 1)]
 
 for _ in range(m):
     x, y = map(int, input().split())
-    graph[x][y] = True
-    graph[y][x] = True
+    graph[x].append(y)
+    graph[y].append(x)
 
-cnt = 0
-result = []
-def DFS(v, cnt):
-    cnt += 1
-    visited[v]= True
-    if v == second:
-        result.append(cnt)
-    else:
-        for i in range(1, n + 1):
-            if visited[i] == False and graph[v][i]:
-                DFS(i, cnt)
+from collections import deque
 
-DFS(first, 0)
-if(len(result) == 0): print(-1)
-else: print(result[0] - 1)
+def BFS(v):
+    queue = deque()
+    queue.append(v)
+    while queue:
+        x = queue.popleft()
+        for i in graph[x]:
+            if visited[i] == -1:
+                visited[i] = visited[x] + 1
+                queue.append(i)
+
+visited[first] = 0
+BFS(first)
+print(visited[second])
+
+# visited =  [False] * (n + 1)
+# graph = [[False] * (n + 1) for _ in range(n + 1)]
+
+# for _ in range(m):
+#     x, y = map(int, input().split())
+#     graph[x][y] = True
+#     graph[y][x] = True
+
+# cnt = 0
+# result = []
+# def DFS(v, cnt):
+#     cnt += 1
+#     visited[v]= True
+#     if v == second:
+#         result.append(cnt)
+#     else:
+#         for i in range(1, n + 1):
+#             if visited[i] == False and graph[v][i]:
+#                 DFS(i, cnt)
+
+# DFS(first, 0)
+# if(len(result) == 0): print(-1)
+# else: print(result[0] - 1)
