a7e763f6aad8d8a9b3e2084ee2e3c8d9bd374c2e
SuJin <kimsujin001127@gmail.com>
2024-04-14 13:46:19 +0900

    백준 BFS 실2

--- Python/05 DFSBFS/11725_트리의부모찾기.py
+++ Python/05 DFSBFS/11725_트리의부모찾기.py
@@ -2,6 +2,8 @@ import sys
 sys.setrecursionlimit(10**6)
 input=sys.stdin.readline
 
+from collections import deque
+
 n = int(input())
 graph = [[] for _ in range(n+1) ]
 visited = [0] * (n + 1)
@@ -9,6 +11,20 @@ for _ in range(n-1):
     x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
+
+def BFS(v):
+    queue = deque()
+    queue.append(v)
+    while queue:
+        x = queue.popleft()
+        for i in graph[x]:
+            if visited[i] == 0:
+                visited[i] = x
+                queue.append(i)
+
+BFS(1)
+for i in range(2, n + 1):
+    print(visited[i])
 
 def DFS(v):
     for i in graph[v]:
