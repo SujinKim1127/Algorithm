b1e75eb19fc1afc1575f4fd5b07991c58de2c021
SuJin <kimsujin001127@gmail.com>
2024-05-23 15:14:50 +0900

    백준 구현 실4

--- Python/04 Implementation/1748_수이어쓰기.py
+++ Python/04 Implementation/1748_수이어쓰기.py
@@ -1,8 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
-value = ''
+n_len = len(str(n))
+count = 0
 
-for i in range(1, n+1):
-    value += str(i)
 
-print(len(value))
+for i in range(n_len - 1):
+    count += 9 * 10 ** i * (i + 1)
+
+
+print(count + (n - 10 ** (n_len - 1) + 1) * n_len)
