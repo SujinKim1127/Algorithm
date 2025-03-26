dc3483f9ad4cf228330eafe13b74c5e1d311202e
SuJin <kimsujin001127@gmail.com>
2025-03-27 06:52:29 +0900

    백준 이진탐색 실3 bisect

--- Python/07 이진탐색/7795_먹을것인가먹힐것인가.py
+++ Python/07 이진탐색/7795_먹을것인가먹힐것인가.py
@@ -1,27 +1,44 @@
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
-t = int(input())
+# t = int(input())
 
-def binary_search(array, target, start, end, count):
-    if start>end:
-        return count
-    mid = (start + end) // 2
-    if target > array[mid]:
-        return binary_search(array, target, mid + 1, end, mid)
-    else:
-        return binary_search(array, target, start, mid - 1, count)
+# def binary_search(array, target, start, end, count):
+#     if start>end:
+#         return count
+#     mid = (start + end) // 2
+#     if target > array[mid]:
+#         return binary_search(array, target, mid + 1, end, mid)
+#     else:
+#         return binary_search(array, target, start, mid - 1, count)
 
-for _ in range(t):
-    m, n = map(int, input().split())
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
-    a.sort()
-    b.sort()
+# for _ in range(t):
+#     m, n = map(int, input().split())
+#     a = list(map(int, input().split()))
+#     b = list(map(int, input().split()))
+#     a.sort()
+#     b.sort()
 
 
-    result = 0
-    for x in a:
-        result += binary_search(b, x, 0, n - 1, -1) + 1
+#     result = 0
+#     for x in a:
+#         result += binary_search(b, x, 0, n - 1, -1) + 1
 
-    print(result)
+#     print(result)
+
+
+
+
+from bisect import bisect_left
+from sys import stdin
+input = stdin.readline
+
+for _ in range(int(input())): 
+    _ = input()  
+    nums = list(map(int, input().split()))  
+    goal = sorted(map(int, input().split())) 
+
+    print(sum(
+        bisect_left(goal, num)  # num보다 작은 원소 개수 sum
+        for num in nums
+    ))
