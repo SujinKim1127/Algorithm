8b630707f442b503c8f83af9590f3c18e52a32a8
kimsujin <kimsujin001127@gmail.com>
2025-09-01 19:03:18 +0900

    백준 DP 실3

---
+++ Python/08 다이나믹 프로그래밍/9657_돌게임3.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [1,1,0,1,1]
+
+for i in range(5, n+1):
+  if dp[i - 1] + dp[i - 3] + dp[i-4] == 3:
+    dp.append(0)
+  else: dp.append(1)
+
+print("SK" if dp[n] else "CY")
