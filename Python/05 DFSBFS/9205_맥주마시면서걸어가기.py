fb1927d267e452ef7be6d0bac75dd25b83b30ae3
kimsujin <kimsujin001127@gmail.com>
2025-09-09 17:54:29 +0900

    백준 BFS 골5

--- Python/05 DFSBFS/9205_맥주마시면서걸어가기.py
+++ Python/05 DFSBFS/9205_맥주마시면서걸어가기.py
@@ -1,15 +1,33 @@
+from collections import deque 
 t = int(input())
+
+
+def bfs():
+    q = deque()
+    q.append((house_x, house_y))
+    while q:
+        x, y = q.popleft()
+        # 락페 가기 가능
+        if abs(x-rock_x) + abs(y-rock_y) <= 1000:
+            print("happy")
+            return
+        for i in range(n):
+            if not visited[i]:  # 방문 안한 편의점
+                new_x, new_y = store[i]
+                if abs(x-new_x) + abs(y - new_y) <= 1000:   # 편의점까지 가는거 가능하면
+                    visited[i] = 1
+                    q.append((new_x, new_y))
+    print("sad")
+    return
+
+
+
 for i in range(t):
-    n = int(input())        # 맥주 파는 편의점 개수
-    house = list(map(int, input().split()))
+    n = int(input())        
+    house_x, house_y = map(int, input().split())
     store = []
     for _ in range(n):
         store.append(list(map(int, input().split())))
-    rock = list(map(int, input().split()))
-
-
-
-
-print(house, store, rock)
-
-
+    rock_x, rock_y = map(int, input().split())
+    visited = [0 for _ in range(n+1)]
+    bfs()
