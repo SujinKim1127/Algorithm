520d4afbb978bf562478560d49d119f06e574e5e
SuJin <kimsujin001127@gmail.com>
2025-11-03 15:16:33 +0900

    leet Greedy Easy

---
+++ Python/03 greedy/2037_MinimumNumberofMovestoSeatEveryone.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats.sort()
+        students.sort()
+        answer = 0
+        for i in range(len(seats)):
+            answer += abs(seats[i] - students[i])
+        
+        return answer
+    
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        return sum([abs(x - y) for x, y in zip(sorted(seats), sorted(students))])
