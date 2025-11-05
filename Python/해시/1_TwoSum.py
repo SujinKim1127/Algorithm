ee96810657353e440efd37432fa0a873b5605241
SuJin <kimsujin001127@gmail.com>
2025-11-05 20:12:54 +0900

    leet 해시 복습

--- Python/해시/1_TwoSum.py
+++ Python/해시/1_TwoSum.py
@@ -7,4 +7,11 @@ class Solution:
                 return [i, hashmap[gap]]
             
             hashmap[nums[i]] = hashmap.get(nums[i], 0) + i
-                        
+        
+        # enumerate 로 풀기
+        for i, num in enumerate(nums):
+            gap = target - num
+            if gap in hashmap:
+                return [i, hashmap[gap]]
+            
+            hashmap[num] = hashmap.get(num, 0) + i
