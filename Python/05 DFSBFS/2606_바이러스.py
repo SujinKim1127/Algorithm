79c6e131e7091746d97e883a907a2b1460cacc0b
SuJin <kimsujin001127@gmail.com>
2025-11-18 14:44:25 +0900

    백준 DFS 실3

--- Python/05 DFSBFS/2606_바이러스.py
+++ Python/05 DFSBFS/2606_바이러스.py
@@ -1,49 +1,74 @@
+# n = int(input())
+# m = int(input())
+
+# visited = [0] * (n+1)
+# graph = [[False] * (n + 1) for _ in range(n+1)]
+
+# for _ in range(m):
+#     x, y = map(int, input().split())
+#     graph[x][y] = True
+#     graph[y][x] = True
+
+# def DFS(v):
+#     visited[v] = 1
+#     for i in range(1, n+1):
+#         if visited[i] == 0 and graph[v][i]:
+#             print("v,i", v,i)
+#             DFS(i)
+
+# DFS(1)
+# print(sum(visited) - 1)
+
+# from collections import deque
+
+# n = int(input())
+# net = int(input())
+
+# visited = [False] * (n+1)
+# graph = [[] for _ in range(n+1)]
+
+# for _ in range(net):
+#     x, y = map(int, input().split())
+#     graph[x].append(y)
+#     graph[y].append(x)
+
+# def BFS(x):
+#     q = deque()
+#     q.append(x)
+#     visited[x]=True
+#     result = 0
+#     while q:
+#         v = q.popleft()
+#         for i in graph[v]:
+#             if not visited[i]:
+#                 q.append(i)
+#                 visited[i] = True
+#                 result += 1
+#     return result
+
+# print(BFS(1))
+
+
 n = int(input())
 m = int(input())
 
-visited = [0] * (n+1)
-graph = [[False] * (n + 1) for _ in range(n+1)]
-
+visited = [False] * (n + 1)
+graph = [[] * (n+1) for _ in range(n+1)]
 for _ in range(m):
-    x, y = map(int, input().split())
-    graph[x][y] = True
-    graph[y][x] = True
-
-def DFS(v):
-    visited[v] = 1
-    for i in range(1, n+1):
-        if visited[i] == 0 and graph[v][i]:
-            print("v,i", v,i)
-            DFS(i)
-
-DFS(1)
-print(sum(visited) - 1)
-
-from collections import deque
-
-n = int(input())
-net = int(input())
-
-visited = [False] * (n+1)
-graph = [[] for _ in range(n+1)]
-
-for _ in range(net):
     x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
 
-def BFS(x):
-    q = deque()
-    q.append(x)
-    visited[x]=True
-    result = 0
-    while q:
-        v = q.popleft()
-        for i in graph[v]:
-            if not visited[i]:
-                q.append(i)
-                visited[i] = True
-                result += 1
-    return result
+count = 0
 
-print(BFS(1))
+def DFS(v):
+    global count
+    visited[v] = True
+    
+    for next in graph[v]:
+        if not visited[next]:
+            count += 1
+            DFS(next)
+
+DFS(1)  # 1번에서 감염 시작
+print(count)
