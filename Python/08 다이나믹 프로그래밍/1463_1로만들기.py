90bd340b4a42efff90e36528b54ed70399e061e7
SuJin <kimsujin001127@gmail.com>
2025-11-22 11:04:34 +0900

    백준 DP 실3

--- Python/08 다이나믹 프로그래밍/1463_1로만들기.py
+++ Python/08 다이나믹 프로그래밍/1463_1로만들기.py
@@ -5,10 +5,13 @@ x = int(input())
 
 dp = [0] * 1000001
 
-for i in range(2,x+1):
-    dp[i] = dp[i-1] + 1
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1)
+for i in range(2, x+1):
+    # 1 빼기는 무조건 가능한 기본 케이스
+    dp[i] = dp[i - 1] + 1
+    
+    if i % 2 == 0: 
+        dp[i] = min(dp[i], dp[i // 2] + 1)
     if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+
 print(dp[x])
